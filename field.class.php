7ca5425426425531c045d38920422f7107495fc0
Duy Huynh <duy.huynh@citynow.vn>
2020-12-24 22:05:46 +0700

    UI for field

--- field.class.php
+++ field.class.php
@@ -36,32 +36,48 @@ class profile_field_myprofilefield extends profile_field_base {
         $myvalue = $DB->get_records('user');
         $text = $mform->addElement('html', ' 
  <link rel="stylesheet" href="/user/profile/field/myprofilefield/style.css">
- <div id="videoCanvas">
-    <video id="camera" autoplay></video>
-
-  </div>
-    <div>
-    <div id="showExacly"></div>
-    <canvas id="photoLeft"></canvas>
-    <canvas id="photoCenter"></canvas>
-    <canvas id="photoRight"></canvas>
-    <canvas id="blank" style="display:none"></canvas>
+ <div id="videoCanvas" style="margin-bottom: 1rem">
+    <p style="margin-right: 90px">Hình xác minh của bạn:  </p><div id="button-snap" class="button" onclick="handleClickOpenCam()">Mở camera của bạn</div>
+    <div id="dontshow">
+        <video width="500" style="display: none" id="camera" autoplay="false"></video>
+        <img width="500" id="loading" src="https://quynhon.flchotelsresorts.com/wp-content/themes/flchotel/assets/loading.gif">
+        <div style="margin-top: 15px">
+            <div class="button-container">
+                <canvas style="border:1px solid" width="100" height="100" id="photoLeft"></canvas>
+                <p id="text-left" class="text">Ảnh trái</p>
+                <div id="recapture-left" onclick="handleResetLeftPicture()" class="button-recapture button-recapture-disable">Chụp lại</div>
+            </div>
+            <div class="button-container">
+                <canvas style="border:1px solid" width="100" height="100" id="photoCenter"></canvas>
+                <p id="text-center" class="text">Ảnh giữa</p>
+                <div id="recapture-center" onclick="handleResetCenterPicture()" class="button-recapture button-recapture-disable">Chụp lại</div>
+            </div>
+            <div class="button-container">
+                <canvas style="border:1px solid" width="100" height="100" id="photoRight"></canvas>
+                <p id="text-right" class="text">Ảnh phải</p>
+                <div id="recapture-right" onclick="handleResetRightPicture()" class="button-recapture button-recapture-disable">Chụp lại</div>
+            </div>
+            <div style="display: flex; margin-top: 10px">
+                <div id="button-submit" class="button button-disable" onclick="handleSubmitPicture()">Lưu ảnh</div>
+                <div class="button" onclick="handleResetPicture()">Chụp lại ảnh khác</div>
+            </div>
+           
+        </div>
     </div>
-
   <script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
   <script src="/user/profile/field/myprofilefield/face-api.js"></script>
 </script>');
-        $PAGE->requires->js('/user/profile/field/myprofilefield/script.js');
+        $PAGE->requires->js('/user/profile/field/myprofilefield/mytest.js');
         $PAGE->requires->js_init_call('init', array($myvalue));
         $PAGE->requires->js_init_call('myuser', array($USER));
-        $text = $mform->addElement('text', $this->inputname, format_string($this->field->name));
+       /* $text = $mform->addElement('text', $this->inputname, format_string($this->field->name));
         $user = new stdClass();
         $user->skype = $this->data;
         $user->id = $USER->id;
         $text->setValue($this->data);
         $DB->update_record('user',$user);
 
-        $mform->setType($this->inputname, PARAM_EMAIL);
+        $mform->setType($this->inputname, PARAM_EMAIL); */
         if ($this->is_required() and !has_capability('moodle/user:update', context_system::instance())) {
             $mform->addRule($this->inputname, get_string('required'), 'nonzero', null, 'client');
         }
--- mytest.js
+++ mytest.js
@@ -30,16 +30,24 @@ let localStream;
 function gotLocalMediaStream(mediaStream) {
     localStream = mediaStream;
     localVideo.srcObject = mediaStream;
+    document.getElementById("loading").style.display="none";
+    document.getElementById("camera").style.display="block";
 }
 
 // Handles error by logging a message to the console with the error message.
 function handleLocalMediaStreamError(error) {
     console.log('navigator.getUserMedia error: ', error);
+    document.getElementById("loading").style.display="none";
+    document.getElementById("camera").style.display="block";
 }
 
 // Initializes media stream.
-navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-    .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+function handleClickOpenCam(){
+    document.getElementById("button-snap").style.display = "none";
+    document.getElementById("dontshow").style.display= "block";
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+        .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+}
 
 var video = document.getElementById('camera');
 
@@ -54,28 +62,64 @@ function grabWebCamVideo() {
         });
 }
 
-//var photo = document.getElementById('photo');
-//var photoContext = photo.getContext('2d');
 const photoCenter = document.getElementById('photoCenter');
 const photoRight = document.getElementById('photoRight');
 const photoLeft = document.getElementById('photoLeft');
-
+const textCenter = document.getElementById('text-center');
+const textRight = document.getElementById('text-right');
+const textLeft = document.getElementById('text-left');
+const recapCenter = document.getElementById('recapture-center');
+const recapRight = document.getElementById('recapture-right');
+const recapLeft = document.getElementById('recapture-left');
+const buttonSubmit = document.getElementById('button-submit');
 
 function snapPhoto(photoSnap,sourceCanvas) {
     const photoContext = photoSnap.getContext('2d');
     photoContext.drawImage(sourceCanvas, 0, 0,100,100);
+    if (!isCanvasBlank(photoRight) && !isCanvasBlank(photoLeft) && !isCanvasBlank(photoCenter)){
+        buttonSubmit.classList.remove("button-disable");
+    }
     //show(photo, sendBtn);
+}
+
+function handleResetPicture(){
+    photoCenter.getContext('2d').clearRect(0, 0, photoCenter.width, photoCenter.height);
+    photoRight.getContext('2d').clearRect(0, 0, photoRight.width, photoRight.height);
+    photoLeft.getContext('2d').clearRect(0, 0, photoLeft.width, photoLeft.height);
+    textCenter.style.display = "block";
+    textRight.style.display = "block";
+    textLeft.style.display = "block";
+}
+
+function handleResetLeftPicture(){
+    buttonSubmit.classList.add("button-disable");
+    photoLeft.getContext('2d').clearRect(0, 0, photoLeft.width, photoLeft.height);
+    textLeft.style.display = "block";
+}
+
+function handleResetRightPicture(){
+    buttonSubmit.classList.add("button-disable");
+    photoRight.getContext('2d').clearRect(0, 0, photoLeft.width, photoLeft.height);
+    textRight.style.display = "block";
+}
+
+function handleResetCenterPicture(){
+    buttonSubmit.classList.add("button-disable");
+    photoCenter.getContext('2d').clearRect(0, 0, photoLeft.width, photoLeft.height);
+    textCenter.style.display = "block";
+}
+
+function handleSubmitPicture(){
+    if ( !isCanvasBlank(photoRight) && !isCanvasBlank(photoLeft) && !isCanvasBlank(photoCenter)) {
+        alert('Ảnh của bạn đã được lưu');
+    }
 }
 
 video.addEventListener("play", async () => {
     const canvas = faceapi.createCanvasFromMedia(video);
     canvas.id = "mycanvas";
-    document.getElementById("videoCanvas").append(canvas);
+    document.getElementById("dontshow").append(canvas);
     const displaySize = { width: 640 , height: 480 };
-
-    const regionsToExtract = [
-        new faceapi.Rect(0, 0, 100, 100)
-    ]
 
     faceapi.matchDimensions(canvas,displaySize);
     setInterval(async () =>{
@@ -93,11 +137,7 @@ video.addEventListener("play", async () => {
             const ry = (eye_left[0] + (eye_right[0] - eye_left[0]) / 2 - nose[0]) /
                 res.detection.box.width;
             const detection = res.detection.box;
-            /*console.log(
-                res.detection.score, //Face detection score
-                ry, //Closest to 0 is looking forward
-                rx // Closest to 0.5 is looking forward, closest to 0 is looking up
-            );*/
+
             const regionsToExtract = [
                 new faceapi.Rect(detection.x, detection.y, detection.width, detection.height)
             ]
@@ -114,27 +154,35 @@ video.addEventListener("play", async () => {
                     if (ry < -0.06) {
                         state = "left";
                         if (isCanvasBlank(photoLeft)){
+                            textLeft.style.display = "none";
+                            recapLeft.style.display="block";
                             snapPhoto(photoLeft, canvases[0]);
                         }
                     }
                     if (ry > 0.06) {
                         state = "right";
                         if (isCanvasBlank(photoRight)){
+                            textRight.style.display = "none";
+                            recapRight.style.display="block";
                             snapPhoto(photoRight, canvases[0]);
                         }
                     }
                     if (ry > -0.002 && ry < 0.002) {
                         state = "center";
                         if (isCanvasBlank(photoCenter)){
+                            textCenter.style.display = "none";
+                            recapCenter.style.display="block";
                             snapPhoto(photoCenter, canvases[0]);
                         }
                     }
                 }
             }
         }
-        canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height);
-        faceapi.draw.drawDetections(canvas,resizeDetections);
-        faceapi.draw.drawFaceLandmarks(canvas,resizeDetections);
+        //clear canvas
+        //canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height);
+
+        //faceapi.draw.drawDetections(canvas,resizeDetections);
+        //faceapi.draw.drawFaceLandmarks(canvas,resizeDetections);
         if (resizeDetections.length>0){
            const detection = resizeDetections[0].detection._box;
         }
@@ -145,61 +193,6 @@ function isCanvasBlank(canvas) {
     return !canvas.getContext('2d')
         .getImageData(0, 0, canvas.width, canvas.height).data
         .some(channel => channel !== 0);
-}
-
-async function leftEyePosition() {
-    faceapi
-        .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks()
-        .then(async(res) => {
-            // Face is detected
-            if (res) {
-                const eye_right = getMeanPosition(res.landmarks.getRightEye());
-                const eye_left = getMeanPosition(res.landmarks.getLeftEye());
-                const nose = getMeanPosition(res.landmarks.getNose());
-                const mouth = getMeanPosition(res.landmarks.getMouth());
-                const jaw = getTop(res.landmarks.getJawOutline());
-
-                const rx = (jaw - mouth[1]) / res.detection.box.height + 0.5;
-                const ry = (eye_left[0] + (eye_right[0] - eye_left[0]) / 2 - nose[0]) /
-                    res.detection.box.width;
-                const detection = res.detection.box;
-                /*console.log(
-                    res.detection.score, //Face detection score
-                    ry, //Closest to 0 is looking forward
-                    rx // Closest to 0.5 is looking forward, closest to 0 is looking up
-                );*/
-                const regionsToExtract = [
-                    new faceapi.Rect(detection.x, detection.y, detection.width, detection.height)
-                ]
-                // actually extractFaces is meant to extract face regions from bounding boxes
-                // but you can also use it to extract any other region
-                const canvases = await faceapi.extractFaces(video, regionsToExtract);
-
-                let state = "undetected";
-                if (res.detection.score > 0.7) {
-                    state = "front";
-                    if (rx > 0.2) {
-                        state = "top";
-                    } else {
-                        if (ry < -0.06) {
-                            state = "left";
-                            snapPhoto(photoLeft,canvases[0]);
-                        }
-                        if (ry > 0.06) {
-                            state = "right";
-                            snapPhoto(photoRight,canvases[0]);
-                        }
-                        if (ry > -0.005 && ry < 0.001) {
-                            state = "right";
-                            snapPhoto(photoCenter,canvases[0]);
-                        }
-                    }
-                }
-            } else {
-                // Face was not detected
-            }
-        })
 }
 
 function getTop(l) {
