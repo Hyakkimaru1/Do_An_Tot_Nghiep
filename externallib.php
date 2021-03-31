0d90690926be5d1aceae379e0d50a7d7309e0f33
quocdung1999 <1712369@student.hcmus.edu.vn>
2021-03-31 11:31:44 +0700

    Added 2 create functions for sessions and logs

--- externallib.php
+++ externallib.php
@@ -54,7 +54,7 @@ class local_webservices_external extends external_api {
     public static function get_a_log_parameters() {
         return new external_function_parameters(
             array(
-                'studentid' => new external_value(PARAM_TEXT, 'student ID parameter'),
+                'studentid' => new external_value(PARAM_INT, 'student ID parameter'),
                 'sessionid'  => new external_value(PARAM_INT, 'session ID parameter'),
             )
         );
@@ -79,7 +79,7 @@ class local_webservices_external extends external_api {
             'sessionid' => $sessionid));
         global $DB;
 
-        $sql = "SELECT l.*, a.course,r.name as room,r.campus
+        $sql = "SELECT l.*, a.course,r.name as room,r.campus, s.lesson
                   FROM {attendance_log} l
             LEFT JOIN {attendance_session} s ON s.id = l.sessionid
             LEFT JOIN {room} r ON r.id = s.roomid
@@ -100,9 +100,10 @@ class local_webservices_external extends external_api {
             new external_single_structure(
                 array(
                     'id' => new external_value(PARAM_INT, 'log ID', VALUE_DEFAULT, null),
-                    'studentid' => new external_value(PARAM_TEXT, 'student ID', VALUE_DEFAULT, null),
-                    'course' => new external_value(PARAM_TEXT, 'course ID', VALUE_DEFAULT, null),
+                    'studentid' => new external_value(PARAM_INT, 'student ID', VALUE_DEFAULT, null),
+                    'course' => new external_value(PARAM_INT, 'course ID', VALUE_DEFAULT, null),
                     'sessionid' => new external_value(PARAM_INT, 'session ID', VALUE_DEFAULT, null),
+                    'lesson' => new external_value(PARAM_INT, 'lesson number', VALUE_DEFAULT, null),
                     'room' => new external_value(PARAM_TEXT, 'room name', VALUE_DEFAULT, null),
                     'campus' => new external_value(PARAM_TEXT, 'campus location', VALUE_DEFAULT, null),
                     'timein' => new external_value(PARAM_INT, 'timestamp when the student checkin', VALUE_DEFAULT, null),
@@ -126,9 +127,10 @@ class local_webservices_external extends external_api {
             )
         );
         global $DB;
-        $sql = "SELECT l.*, a.course
+        $sql = "SELECT l.*, a.course,r.name as room, r.campus, s.lesson
                 FROM {attendance_log} l
-                LEFT JOIN {attendance_session} s ON l.sessionid = s.id 
+                LEFT JOIN {attendance_session} s ON l.sessionid = s.id
+                LEFT JOIN {room} r ON r.id = s.roomid
                 LEFT JOIN {attendance} a 
                 ON s.attendanceid = a.id 
                 WHERE a.id = :attendanceid
@@ -142,11 +144,60 @@ class local_webservices_external extends external_api {
             new external_single_structure(
                 array(
                     'id' => new external_value(PARAM_INT, 'log ID', VALUE_DEFAULT, null),
-                    'studentid' => new external_value(PARAM_TEXT, 'student ID', VALUE_DEFAULT, null),
-                    'course' => new external_value(PARAM_TEXT, 'class ID', VALUE_DEFAULT, null),
+                    'studentid' => new external_value(PARAM_INT, 'student ID', VALUE_DEFAULT, null),
+                    'course' => new external_value(PARAM_INT, 'course ID', VALUE_DEFAULT, null),
                     'sessionid' => new external_value(PARAM_INT, 'session ID', VALUE_DEFAULT, null),
-                    'timein' => new external_value(PARAM_TEXT, 'time when the student checkin', VALUE_DEFAULT, null),
-                    'timeout' => new external_value(PARAM_TEXT, 'time when the student go out', VALUE_DEFAULT, null),
+                    'lesson' => new external_value(PARAM_INT, 'lesson number', VALUE_DEFAULT, null),
+                    'room' => new external_value(PARAM_TEXT, 'room name', VALUE_DEFAULT, null),
+                    'campus' => new external_value(PARAM_TEXT, 'campus location', VALUE_DEFAULT, null),
+                    'timein' => new external_value(PARAM_INT, 'time when the student checkin', VALUE_DEFAULT, null),
+                    'timeout' => new external_value(PARAM_INT, 'time when the student go out', VALUE_DEFAULT, null),
+                    'status' => new external_value(PARAM_INT, 'status of the student', VALUE_DEFAULT, null),
+                )
+            )
+        );
+    }
+
+    public static function get_logs_by_course_id_parameters(): external_function_parameters
+    {
+        return new external_function_parameters(
+            array(
+                'courseid' => new external_value(PARAM_INT, 'Course ID'),
+            )
+        );
+    }
+    public static function get_logs_by_course_id(int $courseid): array
+    {
+        $params = self::validate_parameters(self::get_logs_by_course_id_parameters(), array(
+                'courseid' => $courseid
+            )
+        );
+        global $DB;
+        $sql = "SELECT l.*, r.name as room, r.campus, s.lesson
+                FROM {attendance_log} l
+                LEFT JOIN {attendance_session} s ON l.sessionid = s.id 
+                LEFT JOIN {room} r ON r.id = s.roomid
+                LEFT JOIN {attendance} a 
+                ON s.attendanceid = a.id 
+                WHERE a.course = :courseid
+                ORDER BY l.id ASC";
+
+        return $DB->get_records_sql($sql,array('courseid' => $courseid));
+    }
+
+    public static function get_logs_by_course_id_returns(): external_multiple_structure
+    {
+        return new external_multiple_structure(
+            new external_single_structure(
+                array(
+                    'id' => new external_value(PARAM_INT, 'log ID', VALUE_DEFAULT, null),
+                    'studentid' => new external_value(PARAM_INT, 'student ID', VALUE_DEFAULT, null),
+                    'sessionid' => new external_value(PARAM_INT, 'session ID', VALUE_DEFAULT, null),
+                    'lesson' => new external_value(PARAM_INT, 'lesson number', VALUE_DEFAULT, null),
+                    'room' => new external_value(PARAM_TEXT, 'room name', VALUE_DEFAULT, null),
+                    'campus' => new external_value(PARAM_TEXT, 'campus location', VALUE_DEFAULT, null),
+                    'timein' => new external_value(PARAM_INT, 'timestamp when the student checkin', VALUE_DEFAULT, null),
+                    'timeout' => new external_value(PARAM_INT, 'timestamp when the student checkout', VALUE_DEFAULT, null),
                     'status' => new external_value(PARAM_INT, 'status of the student', VALUE_DEFAULT, null),
                 )
             )
@@ -244,30 +295,210 @@ class local_webservices_external extends external_api {
 
         // Check if this course was created or not
 
-        $sql = "SELECT course.*
+        $sql1 = "SELECT course.*
                 FROM {course} course 
                 WHERE course.id = :courseid";
-        $data = $DB->get_record_sql($sql,array('courseid'=>$courseid),IGNORE_MISSING);
+        $result1 = $DB->get_record_sql($sql1,array('courseid'=>$courseid),IGNORE_MISSING);
 
         $return = array('message' => '');
-        if ($data == false)
+        if ($result1 == false)
         {
             $return['message'] = "There isn't any course with this ID";
         }
         else {
-            $data = (object) array('course'=>$courseid);
+            $sql2 = "SELECT a.*
+                FROM {attendance} a
+                WHERE a.course = :courseid";
+            $result2 = $DB->get_record_sql($sql2,array('courseid'=>$courseid),IGNORE_MISSING);
+            if ($result2 == false) {
+                $data = (object)array('course' => $courseid);
 
-            if ($DB->insert_record('attendance',$data)) {
-                $return['message'] = "Created attendance successfully";
+                if ($DB->insert_record('attendance', $data)) {
+                    $return['message'] = "Created attendance successfully";
+                } else {
+                    $return['message'] = "Couldn't create attedance";
+                }
             }
             else {
-                $return['message'] = "Couldn't create attedance";
+                $return['message'] = "There is already an attendance record for this course";
             }
         }
         return $return;
     }
 
     public static function create_attendance_returns(): external_single_structure
+    {
+        return new external_single_structure(
+            array(
+                'message' => new external_value(PARAM_TEXT, 'Message to the back-end'),
+            )
+        );
+    }
+
+    public static function create_sessions_parameters(): external_function_parameters
+    {
+        return new external_function_parameters(
+            array(
+                'courseid'  => new external_value(PARAM_INT, 'Course ID'),
+                'roomid' => new external_value(PARAM_INT,'Room ID'),
+                'sessdate' => new external_value(PARAM_INT,'Timestamp when the class start'),
+                'lastdate' => new external_value(PARAM_INT,'Timestamp when the class end'),
+                'lesson' => new external_value(PARAM_INT,'Number of lessons or weeks'),
+            )
+
+        );
+    }
+
+    public static function create_sessions(int $courseid,int $roomid, int $sessdate, int $lastdate,
+                                           int $lesson): array
+    {
+        $params = self::validate_parameters(self::create_sessions_parameters(), array(
+                'courseid' => $courseid,
+                'roomid' => $roomid,
+                'sessdate' => $sessdate,
+                'lastdate' => $lastdate,
+                'lesson' => $lesson
+            )
+        );
+
+        global $DB;
+
+        // Check if this course has an attendance record or not
+
+        $sql1 = "SELECT a.*
+                FROM {attendance} a
+                WHERE a.course = :courseid";
+        $result1 = $DB->get_record_sql($sql1,array('courseid'=>$courseid));
+
+        $return = array('message' => '');
+        if ($result1 == false)
+        {
+            $return['message'] = "This course doesn't have an attendance record";
+        }
+        else {
+            // Check if this course has session records or not
+            $sql2 = "SELECT s.*
+                FROM {attendance_session} s
+                LEFT JOIN {attendance} a ON a.id = s.attendanceid
+                WHERE a.course = :courseid";
+            $result2 = $DB->get_record_sql($sql2,array('courseid'=>$courseid),IGNORE_MULTIPLE);
+            if ($result2 == false) {
+                $sql3 = "SELECT r.*
+                FROM {room} r WHERE r.id = :roomid";
+                $result3 = $DB->get_record_sql($sql3,array('roomid'=>$roomid));
+                if ($result3 == false) {
+                    $return['message'] = "There isn't any room with this ID";
+                }
+                else {
+                    $data = array();
+                    for ($i = 1;$i<=$lesson;$i++) {
+                        $data[] = (object)array('attendanceid' => $result1->id,'roomid'=>$roomid,
+                            'sessdate'=>$sessdate,'lastdate'=>$lastdate,'lesson'=>$i);
+                    }
+                    try {
+                        $DB->insert_records('attendance_session', $data);
+                        $return['message'] = "Created sessions successfully";
+                    } catch (coding_exception | dml_exception $e) {
+                        $return['message'] = "Couldn't create sessions";
+                    }
+                }
+            }
+            else {
+                $return['message'] = "There isn't any room with this ID";
+            }
+        }
+        return $return;
+    }
+
+    public static function create_sessions_returns(): external_single_structure
+    {
+        return new external_single_structure(
+            array(
+                'message' => new external_value(PARAM_TEXT, 'Message to the back-end'),
+            )
+        );
+    }
+
+
+    public static function create_logs_parameters(): external_function_parameters
+    {
+        return new external_function_parameters(
+            array(
+                'studentid'  => new external_value(PARAM_INT, 'Student ID'),
+                'courseid' => new external_value(PARAM_INT,'Course ID'),
+                'status' => new external_value(PARAM_INT,'Status number',VALUE_DEFAULT,0),
+            )
+
+        );
+    }
+
+    public static function create_logs(int $studentid, int $courseid, int $status): array
+    {
+        $params = self::validate_parameters(self::create_logs_parameters(), array(
+                'studentid' => $studentid,
+                'courseid' => $courseid,
+                'status' => $status
+            )
+        );
+
+        global $DB;
+
+        // Check if this student was assigned to this course or not
+
+        $sql1 = "SELECT ue.*
+                FROM {user_enrolments} ue
+                LEFT JOIN {enrol} e ON e.id = ue.enrolid
+                WHERE e.courseid = :courseid AND ue.userid = :studentid";
+        $result1 = $DB->get_record_sql($sql1,array('courseid'=>$courseid,'studentid'=>$studentid));
+
+        $return = array('message' => '');
+        if ($result1 == false)
+        {
+            $return['message'] = "This student wasn't assigned to this course / This course or student does not exist";
+        }
+        else {
+            // Check if this course has session records or not
+            $sql2 = "SELECT s.*
+                FROM {attendance_session} s
+                LEFT JOIN {attendance} a ON a.id = s.attendanceid
+                WHERE a.course = :courseid
+                ORDER BY s.id ASC";
+            $result2 = $DB->get_records_sql($sql2,array('courseid'=>$courseid));
+            if ($result2 != []) {
+                    // Check if this student has logs or not
+                    $sql3 = "SELECT l.*
+                    FROM {attendance_log} l
+                    LEFT JOIN {attendance_session} s ON l.sessionid = s.id
+                    LEFT JOIN {attendance} a ON a.id = s.attendanceid
+                    WHERE l.studentid = :studentid AND a.course = :courseid";
+
+                    $result3 = $DB->get_record_sql($sql3,array('studentid'=>$studentid,
+                        'courseid'=>$courseid));
+                    if ($result3 == false) {
+                        $data = array();
+                        foreach ($result2 as $session) {
+                            $data[] = (object)array('studentid' => $studentid, 'sessionid' => $session->id,
+                                'status' => $status);
+                        }
+                        try {
+                            $DB->insert_records('attendance_log', $data);
+                            $return['message'] = "Created logs successfully";
+                        } catch (coding_exception | dml_exception $e) {
+                            $return['message'] = "Couldn't create logs";
+                        }
+                    }
+                    else {
+                        $return['message'] = "There are already log records for this student in this course";
+                    }
+            }
+            else {
+                $return['message'] = "There isn't any session records with this course";
+            }
+        }
+        return $return;
+    }
+
+    public static function create_logs_returns(): external_single_structure
     {
         return new external_single_structure(
             array(
